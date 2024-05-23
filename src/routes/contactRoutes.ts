8abfbd0bcf0a9fc0b7774ee32243fa7752705791
ogcovenant <justcovenant@outlook.com>
2024-05-23 13:50:28 +0100

    finished the add contact route

--- src/routes/contactRoutes.ts
+++ src/routes/contactRoutes.ts
@@ -1,11 +1,16 @@
 //importing auth route packages
 import express from "express";
 import { getContactList } from "../controllers/contactController";
+import { createContact } from "../controllers/contactController";
+import { contactValidationRules } from "../utils/contactValidationRules";
 
 //creating the router instance
 export const router = express.Router();
 
 //route to handle get contact request
-router.route("/").get(getContactList);
+router.route("/")
+  .get(getContactList)
+  .post(contactValidationRules, createContact)
+
 
 export default router;
